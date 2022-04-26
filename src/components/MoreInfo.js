bd0f84a6962519375651c2572500190ae14ca930
Makita7 <sophiamakita@gmail.com>
2022-04-26 15:35:40 -0300

    fixed more info

--- src/components/MoreInfo.js
+++ src/components/MoreInfo.js
@@ -1,23 +1,58 @@
+//when you select a pokemon the info is loaded otherwise there will be a text that says select pokemon
 
-
-function MoreInfo() {
-  return (
+const MoreInfo = ({ data }) => {
+    console.log(data);
+    return (
     <div className="center moreInfo flexbox">
-        <div className="col">
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png" alt="eevee"/>
-        </div>
-        <div className="padTop col">
-            <h1>Eevee</h1>
-            <div className="">
-                <p>Type:</p>
-                <p>HP:</p>
-                <p>Ability:</p>
-                    <ul className="abilities">
-                        <li>Ability One</li>
-                        <li>Ability Two</li>
-                    </ul>
+        {
+        !data ? <h3>Select Pokemon</h3> :
+        
+        <div className="flexbox">
+            <div className="col">
+                <img src={data.sprites.front_default} alt="eevee"/>
+            </div>
+
+            <div className="padTop col">
+                <h1>{data.name}</h1>
+                <div className="">
+                    {
+                        data.types.map(pokemon => {
+                            <p>Type:{pokemon.type}</p>
+                        })
+                    }
+                    <p>Abilities:</p>
+                        {
+                            data.abilities.map(pokemon =>{
+                                return(
+                                    <ul className="abilities">
+                                        <li>{pokemon.ability.name}</li>
+                                    </ul>
+                                );
+                            })
+                        }
+                    
+                        
+                        <p>Weight: {data.weight}</p>
+                </div>
+            </div>
+
+            <div className="col statpad">
+                        {
+                            data.stats.map(pokemon => {
+                                return(
+                                    <>
+                                        <p>{pokemon.stat.name}:{pokemon.base_stat}</p>
+                                    </>
+                                );
+                            })
+                        }
+            </div>
+
+            <div className="col">
+                <img src={data.sprites.front_shiny} alt="eevee"/>
             </div>
         </div>
+        }
     </div>
   );
 }
